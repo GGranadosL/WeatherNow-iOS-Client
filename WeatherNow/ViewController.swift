37dfc9913050a026d9ddcfd9df9243fa5ebec68d
Gerardo Granados <gerardo@albo.mx>
2024-08-22 19:24:36 -0600

    Initial Commit

---
+++ WeatherNow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherNow
+//
+//  Created by Gerardo  Granados Lopez on 22/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
